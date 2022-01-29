3b47e22641122af8d7539256db0e07392fab3815
Mike Litvin <mrlitvinm@gmail.com>
2022-01-29 16:18:21 +1000

    for merge

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
     Vector3 _lastPos;
     public GameObject player;
-    public bool isMoved;
- 
-    void Update ()
+    public bool isMoved; 
+    private NavMeshAgent _agent;
+
+    private void Start()
     {
-        var position = player.transform.position;
-        isMoved = position != _lastPos;
-        _lastPos = position;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+    
+
+    private void Update()
+    {
+
     }
 }
